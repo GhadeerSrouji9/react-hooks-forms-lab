fc732313cc2d4d9d2f2db9a5a2e32e8ec35bff56
Ghadeer Srouji <ghadeersrouji99@gmail.com>
2023-06-11 21:21:14 +0300

    Completed assignment

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,17 +1,25 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
-  return (
-    <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
-        <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
-        <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
-      </select>
-    </div>
-  );
+function Filter({ onCategoryChange, search, setSearch }) {
+    return ( <
+        div className = "Filter" >
+        <
+        input type = "text"
+        placeholder = "Search..."
+        onChange = {
+            (e) => setSearch(e.target.value) }
+        value = { search }
+        /> <
+        select name = "filter"
+        onChange = { onCategoryChange } >
+        <
+        option value = "All" > Filter by category < /option> <
+        option value = "Produce" > Produce < /option> <
+        option value = "Dairy" > Dairy < /option> <
+        option value = "Dessert" > Dessert < /option> <
+        /select> <
+        /div>
+    );
 }
 
 export default Filter;
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,38 @@
 import React from "react";
-import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
-  return (
-    <form className="NewItem">
-      <label>
+function ItemForm({ onItemFormSubmit, onHandleNameChange, onHandleNewCategory, nameInput, categoryInput }) {
+    return ( <
+        form className = "NewItem"
+        onSubmit = { onItemFormSubmit } >
+        <
+        label >
         Name:
-        <input type="text" name="name" />
-      </label>
+        <
+        input onChange = { onHandleNameChange }
+        type = "text"
+        name = "name"
+        value = { nameInput }
+        /> <
+        /label>
 
-      <label>
+        <
+        label >
         Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
+        <
+        select onChange = { onHandleNewCategory }
+        value = { categoryInput }
+        name = "category" >
+        <
+        option value = "Produce" > Produce < /option> <
+        option value = "Dairy" > Dairy < /option> <
+        option value = "Dessert" > Dessert < /option> <
+        /select> <
+        /label>
 
-      <button type="submit">Add to List</button>
-    </form>
-  );
+        <
+        button type = "submit" > Add to List < /button> <
+        /form>
+    );
 }
 
 export default ItemForm;
